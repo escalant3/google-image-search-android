a56442921e7039427271e84525e23eaeb3c15f16
Diego Escalante <diego@bitstrips.com>
2015-09-26 13:15:45 -0400

    Basic lorempixel image loading with infinite scrolling

--- app/src/main/java/com/escalant3/googleimagesearchapp/MainActivity.java
+++ app/src/main/java/com/escalant3/googleimagesearchapp/MainActivity.java
@@ -1,37 +1,47 @@
 package com.escalant3.googleimagesearchapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+import com.escalant3.googleimagesearchapp.adapters.ImagesArrayAdapter;
+import com.escalant3.googleimagesearchapp.listeners.InfiniteScrollListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView gridView;
+    private int counter = 0;
+    ArrayList<String> items = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridView = (GridView) findViewById(R.id.grid_view);
+
+        // DELETE-ME Dummy data
+        addNMoreElements(8);
+
+        // Attach gridView with ImageAdapter
+        gridView.setAdapter(new ImagesArrayAdapter(this, items));
+
+        gridView.setOnScrollListener(new InfiniteScrollListener() {
+            @Override
+            public void onLastItemShown() {
+                onNewItemsLoadStart();
+                addNMoreElements(8);
+                onNewItemsLoadEnd();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void addNMoreElements(int n) {
+        for (int i = 0; i < n; i++) {
+            items.add("http://lorempixel.com/300/300/sports/" + counter);
+            counter++;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/escalant3/googleimagesearchapp/adapters/ImagesArrayAdapter.java
@@ -0,0 +1,56 @@
+package com.escalant3.googleimagesearchapp.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.escalant3.googleimagesearchapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImagesArrayAdapter extends ArrayAdapter<String> {
+
+    private static class ViewHolder {
+        ImageView imageView;
+    }
+
+    public ImagesArrayAdapter(Context context, List<String> items) {
+        super(context, 0, items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        String item = getItem(position);
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.image_item, viewGroup, false);
+            viewHolder.imageView = (ImageView) convertView.findViewById(R.id.image);
+            convertView.setTag(viewHolder);
+
+            // The GridView automatically resizes width to fill columns.
+            // Use that width as the height to have perfect dynamic squares
+            GridView gv = (GridView) viewGroup;
+            int columWidth = gv.getWidth() / gv.getNumColumns();
+            convertView.setLayoutParams(new GridView.LayoutParams(GridView.AUTO_FIT, columWidth));
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // Reset the current image if any before recycling
+        viewHolder.imageView.setImageBitmap(null);
+
+        Picasso.with(viewHolder.imageView.getContext())
+                .load(item)
+                .placeholder(R.mipmap.ic_launcher)
+                .into(viewHolder.imageView);
+
+        return convertView;
+    }
+}
