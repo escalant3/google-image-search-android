03000e71026e18a780ff0fefe34fdd672526e8dc
Diego Escalante <diego@bitstrips.com>
2015-09-26 14:10:05 -0400

    Add search capability to activity

--- app/src/main/java/com/escalant3/googleimagesearchapp/MainActivity.java
+++ app/src/main/java/com/escalant3/googleimagesearchapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.escalant3.googleimagesearchapp;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.View;
 import android.widget.GridView;
 
 import com.escalant3.googleimagesearchapp.adapters.ImagesArrayAdapter;
@@ -11,7 +17,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SearchView searchView;
     private GridView gridView;
+
     private int counter = 0;
     ArrayList<String> items = new ArrayList<>();
 
@@ -20,10 +28,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loadSearchWidget();
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            searchView.setQueryHint("Showing results for: " + query);
+            initGridViewForQuery(query);
+        }
+    }
+
+    private void loadSearchWidget() {
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) findViewById(R.id.search);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                initGridViewForQuery(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    private void initGridViewForQuery(final String query) {
+        counter = 0;
+        items.clear();
+
         gridView = (GridView) findViewById(R.id.grid_view);
 
         // DELETE-ME Dummy data
-        addNMoreElements(8);
+        addNMoreElements(query, 8);
 
         // Attach gridView with ImageAdapter
         gridView.setAdapter(new ImagesArrayAdapter(this, items));
@@ -32,15 +74,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onLastItemShown() {
                 onNewItemsLoadStart();
-                addNMoreElements(8);
+                addNMoreElements(query, 8);
                 onNewItemsLoadEnd();
             }
         });
     }
 
-    private void addNMoreElements(int n) {
+    private void addNMoreElements(String query, int n) {
         for (int i = 0; i < n; i++) {
-            items.add("http://lorempixel.com/300/300/sports/" + counter);
+            items.add("http://lorempixel.com/300/300/" + query + "/" + counter);
             counter++;
         }
     }
